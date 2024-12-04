d8269e897aa1ffd3fc2836a436404f1b24cd2da9
byeong <qudcks4885@gmail.com>
2024-12-04 18:04:29 +0900

    feat: localstorage 키 상수화

---
+++ packages/client/src/shared/constants/guestLocalStorageKey.ts
@@ -0,0 +1,9 @@
+export const GUEST_LOCAL_STORAGE_KEYS = [
+  'isQuizEnd',
+  'reactionStats',
+  'participantStatistics',
+  'hasSubmitted',
+  'submitOrder',
+  'ramainingTime',
+  'currentOrder',
+];
---
+++ packages/client/src/shared/constants/masterLocalStorageKey.ts
@@ -0,0 +1,6 @@
+export const MASTER_LOCAL_STORAGE_KEYS = [
+  'masterStatistics',
+  'reactionStats',
+  'history',
+  'remainingTime',
+];
