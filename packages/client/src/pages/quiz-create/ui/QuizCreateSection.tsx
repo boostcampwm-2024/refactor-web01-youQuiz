159822225aea893cb0605d5a70443def660dac51
dooohun <ehgns0305@naver.com>
2025-01-08 17:00:50 +0900

    refacotr: quizCreateSection 분리

--- packages/client/src/pages/quiz-create/ui/QuizCreateSection.tsx
+++ packages/client/src/pages/quiz-create/ui/QuizCreateSection.tsx
@@ -5,22 +5,23 @@ import InputBox from '@/shared/ui/input-box/InputBox';
 import AnswerBox from './AnswerBox';
 import TimeSelectBox from './TimeSelectBox';
 import PlusIcon from '@/shared/assets/icons/plus.svg?react';
+import { useQuizContext } from '../contexts/useQuizContext';
+import { QuizData } from '../contexts/quizContext.types';
 
-import { QuizData } from '../index';
+export default function QuizCreateSection() {
+  const { quizzes, currentQuizIndex, setQuizzes } = useQuizContext();
+  const quizData = quizzes[currentQuizIndex];
 
-interface QuizCreateSectionProps {
-  quizData: QuizData;
-  currentQuizIndex: number;
-  onQuizUpdate: (updatedData: QuizData) => void;
-}
-
-export default function QuizCreateSection({
-  quizData,
-  currentQuizIndex,
-  onQuizUpdate,
-}: QuizCreateSectionProps) {
   const [showTimeSelect, setShowTimeSelect] = useState(false);
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const onQuizUpdate = (updatedData: QuizData) => {
+    setQuizzes((prev) => {
+      const newQuizzes = [...prev];
+      newQuizzes[currentQuizIndex] = { ...updatedData, position: currentQuizIndex };
+      return newQuizzes;
+    });
+  };
 
   const handleKeyDown = (index: number, e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
@@ -41,7 +42,7 @@ export default function QuizCreateSection({
   return (
     <section>
       <article className="min-w-content min-h-[532px] flex flex-col gap-1 items-center bg-white rounded-base p-6">
-        <p className="self-start relative">
+        <div className="self-start relative">
           <span className="text-weak-md mr-3">{`${currentQuizIndex + 1}번 퀴즈`}</span>
           <span
             className="text-weak-sm cursor-pointer"
@@ -56,15 +57,15 @@ export default function QuizCreateSection({
             />
           )}
           <span className="text-sm text-gray-500 ml-5">* 복수 선택 가능</span>
-        </p>
-        <p className="w-full">
+        </div>
+        <div className="w-full">
           <InputBox
             placeholder="문제를 입력해주세요"
             type="box"
             onSubmit={(value) => onQuizUpdate({ ...quizData, content: value })}
             initialValue={quizData.content}
           />
-        </p>
+        </div>
         <div className="flex flex-col gap-4 w-full mt-6">
           {quizData.choices.map((choice, index) => (
             <AnswerBox
