88ddfa14dd14a0a83078f8d4092c1046e9526cfe
byeong <qudcks4885@gmail.com>
2025-01-14 16:06:36 +0900

    refactor: 페이지 컴포넌트 <CustomSuspense> 컴포넌트로 감싸줌

--- packages/client/src/app/routes/Router.tsx
+++ packages/client/src/app/routes/Router.tsx
@@ -13,25 +13,77 @@ const Leaderboard = lazy(() => import('@/pages/leaderboard'));
 const QuizListPage = lazy(() => import('@/pages/quiz-list'));
 const QuizWaitPage = lazy(() => import('@/pages/quiz-wait'));
 import PreventGuestRouter from './PreventGuestRouter';
+import SkeletonQuizList from '@/pages/quiz-list/ui/SkeletonQuizList';
+import SkeletonQuizWait from '@/pages/quiz-wait/ui/SkeletonQuizWait';
+import CustomSuspense from '@/shared/boundary/\bCustomSuspense';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route element={<HostLayout />}>
-        <Route path="/quiz-list" element={<QuizListPage />} />
-        <Route path="/quiz/create/:classId" element={<QuizCreatePage />} />
+        <Route
+          path="/quiz-list"
+          element={
+            <CustomSuspense fallback={<SkeletonQuizList />}>
+              <QuizListPage />
+            </CustomSuspense>
+          }
+        />
+        <Route
+          path="/quiz/create/:classId"
+          element={
+            <CustomSuspense>
+              <QuizCreatePage />
+            </CustomSuspense>
+          }
+        />
       </Route>
       <Route element={<GuestLayout />}>
         <Route element={<PreventGuestRouter />}>
-          <Route path="/quiz/wait/:pinCode" element={<QuizWaitPage />} />
+          <Route
+            path="/quiz/wait/:pinCode"
+            element={
+              <CustomSuspense fallback={<SkeletonQuizWait />}>
+                <QuizWaitPage />
+              </CustomSuspense>
+            }
+          />
         </Route>
-        <Route path="/nickname/:pinCode" element={<Nickname />} />
+        <Route
+          path="/nickname/:pinCode"
+          element={
+            <CustomSuspense>
+              <Nickname />
+            </CustomSuspense>
+          }
+        />
       </Route>
 
-      <Route path="/quiz/session/:pinCode/:id" element={<QuizSession />} />
-      <Route path="/quiz/session/host/:pinCode/:id" element={<QuizMasterSession />} />
-      <Route path="/quiz/session/:pinCode/end" element={<Leaderboard />} />
+      <Route
+        path="/quiz/session/:pinCode/:id"
+        element={
+          <CustomSuspense>
+            <QuizSession />
+          </CustomSuspense>
+        }
+      />
+      <Route
+        path="/quiz/session/host/:pinCode/:id"
+        element={
+          <CustomSuspense>
+            <QuizMasterSession />
+          </CustomSuspense>
+        }
+      />
+      <Route
+        path="/quiz/session/:pinCode/end"
+        element={
+          <CustomSuspense>
+            <Leaderboard />
+          </CustomSuspense>
+        }
+      />
       <Route path={'*'} element={<NotFound />} />
     </Routes>
   );
