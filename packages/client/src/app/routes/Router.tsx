ea75ea55efedead2d08fc1217eba8addd0e2e4e9
byeong <qudcks4885@gmail.com>
2025-01-16 16:03:10 +0900

    feat: witLazySuspense 고차 컴포넌트 구현
    -고차 컴포넌트 내부에서 lazy import를 수행하고 import한 컴포넌트를 Custom Suspense로 감싸는 구조이다.

--- packages/client/src/app/routes/Router.tsx
+++ packages/client/src/app/routes/Router.tsx
@@ -1,89 +1,38 @@
-import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import withLazySuspense from './withLazySuspense';
 import HostLayout from '@/app/layouts/HostLayout';
 import MainPage from '@/pages/main';
 import QuizCreatePage from '@/pages/quiz-create';
 import GuestLayout from '@/app/layouts/GuestLayout';
 import NotFound from '@/app/routes/NotFound';
 import Nickname from '@/pages/nickname';
-const QuizSession = lazy(() => import('@/pages/quiz-session'));
-const QuizMasterSession = lazy(() => import('@/pages/quiz-master-session'));
-const Leaderboard = lazy(() => import('@/pages/leaderboard'));
-const QuizListPage = lazy(() => import('@/pages/quiz-list'));
-const QuizWaitPage = lazy(() => import('@/pages/quiz-wait'));
+const QuizSession = withLazySuspense(() => import('@/pages/quiz-session'));
+const QuizMasterSession = withLazySuspense(() => import('@/pages/quiz-master-session'));
+const Leaderboard = withLazySuspense(() => import('@/pages/leaderboard'));
+const QuizListPage = withLazySuspense(() => import('@/pages/quiz-list'), <SkeletonQuizList />);
+const QuizWaitPage = withLazySuspense(() => import('@/pages/quiz-wait'), <SkeletonQuizWait />);
 import PreventGuestRouter from './PreventGuestRouter';
 import SkeletonQuizList from '@/pages/quiz-list/ui/SkeletonQuizList';
 import SkeletonQuizWait from '@/pages/quiz-wait/ui/SkeletonQuizWait';
-import CustomSuspense from '@/shared/boundary/\bCustomSuspense';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route element={<HostLayout />}>
-        <Route
-          path="/quiz-list"
-          element={
-            <CustomSuspense fallback={<SkeletonQuizList />}>
-              <QuizListPage />
-            </CustomSuspense>
-          }
-        />
-        <Route
-          path="/quiz/create/:classId"
-          element={
-            <CustomSuspense>
-              <QuizCreatePage />
-            </CustomSuspense>
-          }
-        />
+        <Route path="/quiz-list" element={<QuizListPage />} />
+        <Route path="/quiz/create/:classId" element={<QuizCreatePage />} />
       </Route>
       <Route element={<GuestLayout />}>
         <Route element={<PreventGuestRouter />}>
-          <Route
-            path="/quiz/wait/:pinCode"
-            element={
-              <CustomSuspense fallback={<SkeletonQuizWait />}>
-                <QuizWaitPage />
-              </CustomSuspense>
-            }
-          />
+          <Route path="/quiz/wait/:pinCode" element={<QuizWaitPage />} />
         </Route>
-        <Route
-          path="/nickname/:pinCode"
-          element={
-            <CustomSuspense>
-              <Nickname />
-            </CustomSuspense>
-          }
-        />
+        <Route path="/nickname/:pinCode" element={<Nickname />} />
       </Route>
-
-      <Route
-        path="/quiz/session/:pinCode/:id"
-        element={
-          <CustomSuspense>
-            <QuizSession />
-          </CustomSuspense>
-        }
-      />
-      <Route
-        path="/quiz/session/host/:pinCode/:id"
-        element={
-          <CustomSuspense>
-            <QuizMasterSession />
-          </CustomSuspense>
-        }
-      />
-      <Route
-        path="/quiz/session/:pinCode/end"
-        element={
-          <CustomSuspense>
-            <Leaderboard />
-          </CustomSuspense>
-        }
-      />
+      <Route path="/quiz/session/:pinCode/:id" element={<QuizSession />} />
+      <Route path="/quiz/session/host/:pinCode/:id" element={<QuizMasterSession />} />
+      <Route path="/quiz/session/:pinCode/end" element={<Leaderboard />} />
       <Route path={'*'} element={<NotFound />} />
     </Routes>
   );
