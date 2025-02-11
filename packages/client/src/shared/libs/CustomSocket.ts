84cec73a38539508c2569a3134daffdac0da68e1
byeong <qudcks4885@gmail.com>
2025-02-11 20:44:21 +0900

    fix: CustomButton "Disable" 옵션 추가
    - props로 disable옵션을 주어 버튼 활성/비활성화 상태 관리가능하도록 함

---
+++ packages/client/src/shared/libs/CustomSocket.ts
@@ -0,0 +1,49 @@
+export default class CustomSocket extends WebSocket {
+  constructor(url: string, protocols?: []) {
+    super(url, protocols);
+  }
+
+  public emit(eventName: string, args?: any, cb?: (data: unknown) => unknown) {
+    const data = {
+      eventName,
+      ...args,
+    };
+    this.send(data);
+  }
+
+  public on(eventName: string, cb?: (data: unknown) => unknown) {
+    const handler = (event: MessageEvent) => {
+      if (event.data.eventName === eventName) {
+        if (cb) {
+          cb(event.data);
+          return;
+        }
+        return event.data;
+      }
+    };
+
+    this.addEventListener('message', handler);
+
+    if (cb) {
+      return () => this.removeEventListener('message', handler);
+    }
+  }
+  // MessageEvent<any>
+  public emitWithAck(eventName: string, args?: any, cb?: (data: unknown) => unknown) {
+    return new Promise((resolve, reject) => {
+      this.send({ eventName, ...args });
+      this.addEventListener('message', (event) => {
+        if (event.data.eventName === eventName) {
+          if (cb) {
+            resolve(cb(event.data));
+          } else {
+            resolve(event.data);
+          }
+        }
+      });
+      setTimeout(() => {
+        reject(new Error('no response from server'));
+      }, 3000);
+    });
+  }
+}
