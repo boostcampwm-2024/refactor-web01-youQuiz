659b017d8c024cdfd707b21ce4bb9cbc0f40179b
byeong <qudcks4885@gmail.com>
2025-02-18 17:50:57 +0900

    feat: 추가질의 커스텀 훅 구현

--- packages/client/src/shared/hooks/quizzes.ts
+++ packages/client/src/shared/hooks/quizzes.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { createAIQuiz, createQuiz, createAIChoices } from '../api/quizzes';
+import { createAIQuiz, createQuiz, createAIChoices, requestAdditionalQuiz } from '../api/quizzes';
 import { QuizData } from '@/pages/quiz-create/contexts/quizContext.types';
 import { toastController } from '@/features/toast/model/toastController';
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +11,11 @@ interface Quizzes {
 interface CreateQuizParams {
   quizData: Quizzes;
   classId: number;
+}
+
+interface PromptHistory {
+  role: string;
+  text: string;
 }
 
 export const useCreateQuiz = () => {
@@ -42,13 +47,30 @@ export const useCreateAIQuiz = () => {
   return useMutation({
     mutationKey: ['quiz', 'AI'],
     mutationFn: ({ classId, text }: CreateAIQuizParams) => createAIQuiz(classId, text),
+    onMutate: ({ classId, text }) => {
+      const previousHistory =
+        (queryClient.getQueryData(['promptHistory', classId]) as string[]) || [];
+
+      queryClient.setQueryData(
+        ['promptHistory', classId],
+        [...previousHistory, { role: 'user', text }],
+      );
+
+      return { previousHistory };
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['classes'],
       });
       toast.success('AI가 자동 생성한 퀴즈가 생성되었습니다.');
     },
-    onError: () => toast.error('AI 퀴즈 생성에 실패했습니다.'),
+    onError: (err, variables, context) => {
+      if (context?.previousHistory) {
+        queryClient.setQueryData(['promptHistory', variables.classId], context.previousHistory);
+      }
+      console.log(err);
+      toast.error('AI 퀴즈 생성에 실패했습니다.');
+    },
   });
 };
 
@@ -61,5 +83,29 @@ export const useCreateChoices = () => {
       toast.success('AI가 자동 생성한 선택지가 생성되었습니다.');
     },
     onError: () => toast.error('AI 선택지 생성에 실패했습니다.'),
+  });
+};
+
+export const useRequestAdditionalQuiz = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ['quiz', 'AI', 'additional'],
+    mutationFn: ({ classId, text }: CreateAIQuizParams) => {
+      const previousHistory =
+        (queryClient.getQueryData(['promptHistory', classId]) as PromptHistory[]) || [];
+
+      return requestAdditionalQuiz(classId, [...previousHistory, { role: 'user', text }]);
+    },
+    onMutate: ({ classId, text }) => {
+      const previousHistory =
+        (queryClient.getQueryData(['promptHistory', classId]) as string[]) || [];
+
+      queryClient.setQueryData(
+        ['promptHistory', classId],
+        [...previousHistory, { role: 'user', text }],
+      );
+
+      return { previousHistory };
+    },
   });
 };
