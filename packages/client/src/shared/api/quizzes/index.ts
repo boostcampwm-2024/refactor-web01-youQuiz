18f5713e25cd71c00ce2931fd582fb007fb843ff
byeong <qudcks4885@gmail.com>
2025-02-18 17:50:40 +0900

    feat: 추가질의 API 구현

--- packages/client/src/shared/api/quizzes/index.ts
+++ packages/client/src/shared/api/quizzes/index.ts
@@ -5,6 +5,11 @@ interface Quizzes {
   quizzes: QuizData[];
 }
 
+interface PromptHistory {
+  role: string;
+  text: string;
+}
+
 export const getQuiz = async (classId: number): Promise<QuizData[]> => {
   return apiClient.get(`/classes/${classId}/quizzes`);
 };
@@ -25,5 +30,11 @@ export const createAIChoices = async (classId: number, choices: any) => {
   console.log(choices);
   return apiClient.post(`/classes/${classId}/quizzes/choices/ai/generate`, {
     body: { ...choices },
+  });
+};
+
+export const requestAdditionalQuiz = async (classId: number, promptHistory: PromptHistory[]) => {
+  return apiClient.post(`/classes/${classId}/quizzes/ai/generate/adjust`, {
+    body: { conversationHistory: promptHistory },
   });
 };
